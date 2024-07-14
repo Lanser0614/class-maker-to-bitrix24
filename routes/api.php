fc390f02e18f6ec51760792f6df328da7d2cfdf0
AnvarovD <doniyer.anvarov@alif.uz>
2024-07-14 23:11:16 +0500

    fix

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClassMakerWebhook\ClassMackerApiWebhookController;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::any('/', function () {
+    return new JsonResponse(['data' => "test"], 200);
 });
 
 Route::any('/classmaker-webhook', [ClassMackerApiWebhookController::class, 'webHook']);
